b37f4e85ad774a0c9cffbbbbf3c5c8a6a2a3a77a
jakubwerwinski <werwa999@wp.pl>
2021-11-28 01:36:20 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,69 @@
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <MPU9250.h>
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+MPU9250 mpu;
+
+class MyCallbacks: public BLECharacteristicCallbacks
+{
+    void onWrite(BLECharacteristic *pCharacteristic)
+    {
+        std::string value = pCharacteristic->getValue();
+
+        if (value.length() > 0)
+        {
+            Serial.print("New value: ");
+            for (int i = 0; i < value.length(); i++)
+            {
+                Serial.print(value[i]);
+            }
+            Serial.println();
+
+        }
+    }
+};
+
+void setup()
+{
+
+    Serial.begin(115200);
+    pinMode(22,OUTPUT);
+    digitalWrite(22,LOW);
+    Wire.begin();
+    delay(2000);
+    mpu.setup(0x68);  // change to your own address
+
+
+    BLEDevice::init("ROCKET_PUT");
+    BLEServer *pServer = BLEDevice::createServer();
+
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+
+    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+            CHARACTERISTIC_UUID,
+            BLECharacteristic::PROPERTY_READ |
+            BLECharacteristic::PROPERTY_WRITE
+    );
+
+    pCharacteristic->setCallbacks(new MyCallbacks());
+
+    pCharacteristic->setValue("Hello World");
+    pService->start();
+
+    BLEAdvertising *pAdvertising = pServer->getAdvertising();
+    pAdvertising->start();
+}
+
+void loop()
+{
+    delay(2000);
+    if (mpu.update()) {
+        Serial.print(mpu.getYaw()); Serial.print(", ");
+        Serial.print(mpu.getPitch()); Serial.print(", ");
+        Serial.println(mpu.getRoll());
+    }
+}
